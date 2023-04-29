5edf77d907d6cfc486e328ff4aaf0ee1d97e11fd
DeltaMonkey <engincanozkan@outlook.com>
2023-04-29 11:00:45 +0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import k from './kaboom';
+
+k.scene('game', () => { return {} });
+
+k.go('game');
---
+++ src/kaboom.ts
@@ -0,0 +1,11 @@
+import kaboom from 'kaboom';
+
+const k = kaboom({
+    width: 320,
+    height: 240,
+    scale: 2,
+    background: [0,0,0,1],
+    debug: true
+});
+
+export default k;
